6dd906c6f08b520239d7619ab4fcd656dadb2bf7
ananzhusen <2459622492@qq.com>
2021-10-08 11:53:47 +0800

    anan_add_复选框和单选框

--- packages/form-diagram/index.ts
+++ packages/form-diagram/index.ts
@@ -1,8 +1,14 @@
 export * from './table';
 export * from './checkbox';
+export * from './radio';
 export * from './leSwitch';
 export * from './progress';
+export * from './radioItem';
+export * from './checkboxItem';
+
 import { table } from './table';
+import { checkboxItem } from './checkboxItem';
+import { radioItem } from './radioItem';
 import { checkbox } from './checkbox';
 import { radio } from './radio';
 import { leSwitch } from './leSwitch';
@@ -10,15 +16,12 @@ import { progress } from './progress';
 
 export function formPens() {
   return {
-    table,
-  };
-}
-
-export function formPensbyCtx() {
-  return {
     radio,
     leSwitch,
     progress,
     checkbox,
+    table,
+    radioItem,
+    checkboxItem,
   };
 }
