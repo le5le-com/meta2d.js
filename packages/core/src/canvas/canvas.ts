9626467def286ce0a38beb33f35eeac0d0804537
SignDawn <704425540@qq.com>
2021-10-12 16:56:09 +0800

    先绘制背景色，再绘制网格

--- packages/core/src/canvas/canvas.ts
+++ packages/core/src/canvas/canvas.ts
@@ -1814,15 +1814,15 @@ export class Canvas {
     this.lastRender = now;
     const offscreenCtx = this.offscreen.getContext('2d');
     offscreenCtx.clearRect(0, 0, this.offscreen.width, this.offscreen.height);
-    this.renderGrid();
-    offscreenCtx.save();
-    offscreenCtx.translate(this.store.data.x, this.store.data.y);
     if (this.store.data.background) {
       offscreenCtx.save();
       offscreenCtx.fillStyle = this.store.data.background;
       offscreenCtx.fillRect(0, 0, this.canvas.width, this.canvas.height);
       offscreenCtx.restore();
     }
+    this.renderGrid();
+    offscreenCtx.save();
+    offscreenCtx.translate(this.store.data.x, this.store.data.y);
     window && window.debugRender && console.time('renderPens');
     this.renderPens();
     window && window.debugRender && console.timeEnd('renderPens');
