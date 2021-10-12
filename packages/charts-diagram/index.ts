e4e1e5965b1547095a447d7547e38faea6ad1141
ananzhusen <2459622492@qq.com>
2021-10-12 17:41:31 +0800

    anan_add_仪表盘

--- packages/charts-diagram/index.ts
+++ packages/charts-diagram/index.ts
@@ -1,6 +1,4 @@
 //le5le charts
-export * from './src/instrumentOne';
-export * from './src/instrumentTwo';
 export * from './src/lineChart';
 export * from './src/pieChart';
 export * from './src/histogram';
--- packages/charts-diagram/src/common.ts
+++ packages/charts-diagram/src/common.ts
@@ -5,6 +5,6 @@ export function getValidValue(num: any, value: number) {
   if (value === -1) {
     return num;
   }
-  // return num - parseInt(num) == 0 ? num : Number(num).toFixed(value);
-  return Number(num).toFixed(value);
+  return num - parseInt(num) == 0 ? num : Number(num).toFixed(value);
+  // return Number(num).toFixed(value);
 }
