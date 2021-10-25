aa68b8b3fe2fa88140349960c7141fed6adca9b4
ananzhusen <2459622492@qq.com>
2021-10-25 13:41:29 +0800

    anan_add_checkbox&radio_增加或者减少选项

--- packages/form-diagram/src/radio.ts
+++ packages/form-diagram/src/radio.ts
@@ -65,37 +65,79 @@ function onValue(pen: any) {
   let y = pen.calculative.worldRect.y;
   let w = pen.calculative.worldRect.width;
   let h = pen.calculative.worldRect.height;
-
+  let length = pen.options.length;
   if (pen.direction == 'horizontal') {
-    let length = pen.options.length;
     for (let i = 0; i < length; i++) {
-      let childPen: any = {
-        id: pen.children[i],
-        x: (i * w) / length / w,
-        y: 0,
-        width: 1 / length,
-        height: 1,
-        isChecked: pen.selection === pen.options[i],
-        text: pen.options[i],
-        textLeft: (h * 6) / 5,
-      };
-      pen.calculative.canvas.parent.setValue(childPen);
+      if (pen.children[i]) {
+        let childPen: any = {
+          id: pen.children[i],
+          x: (i * w) / length / w,
+          y: 0,
+          width: 1 / length,
+          height: 1,
+          isChecked: pen.selection === pen.options[i],
+          text: pen.options[i],
+          textLeft: (h * 6) / 5,
+        };
+        pen.calculative.canvas.parent.setValue(childPen);
+      } else {
+        let childPen: any = {
+          name: 'radioItem',
+          x: x + (i * w) / length,
+          y: y,
+          width: w / length,
+          height: h,
+          isChecked: pen.selection === pen.options[i],
+          text: pen.options[i],
+          textLeft: (h * 6) / 5,
+          fillColor: '#1890ff',
+        };
+        pen.calculative.canvas.makePen(childPen);
+        pen.calculative.canvas.parent.pushChildren(pen, [childPen]);
+      }
     }
   } else if (pen.direction == 'vertical') {
-    let length = pen.options.length;
     for (let i = 0; i < length; i++) {
-      let childPen: any = {
-        id: pen.children[i],
-        x: 0,
-        y: (((i * h) / (length * 2 - 1)) * 2) / h,
-        width: 1,
-        height: 1 / (length * 2 - 1),
-        isChecked: pen.selection === pen.options[i],
-        text: pen.options[i],
-        textLeft: ((h / (length * 2 - 1)) * 6) / 5,
-      };
-      pen.calculative.canvas.parent.setValue(childPen);
+      if (pen.children[i]) {
+        let childPen: any = {
+          id: pen.children[i],
+          x: 0,
+          y: (((i * h) / (length * 2 - 1)) * 2) / h,
+          width: 1,
+          height: 1 / (length * 2 - 1),
+          isChecked: pen.selection === pen.options[i],
+          text: pen.options[i],
+          textLeft: ((h / (length * 2 - 1)) * 6) / 5,
+        };
+        pen.calculative.canvas.parent.setValue(childPen);
+      } else {
+        let childPen: any = {
+          name: 'radioItem',
+          x: x,
+          y: y + ((i * h) / (length * 2 - 1)) * 2,
+          width: w,
+          height: h / (length * 2 - 1),
+          isChecked: pen.selection === pen.options[i],
+          text: pen.options[i],
+          textLeft: ((h / (length * 2 - 1)) * 6) / 5,
+          fillColor: '#1890ff',
+        };
+        pen.calculative.canvas.makePen(childPen);
+        pen.calculative.canvas.parent.pushChildren(pen, [childPen]);
+      }
     }
+  }
+
+  if (length < pen.children.length) {
+    pen.children.splice(length, pen.children.length).forEach((p) => {
+      const i = pen.calculative.canvas.parent.store.data.pens.findIndex(
+        (item) => item.id === p
+      );
+      if (i > -1) {
+        pen.calculative.canvas.parent.store.data.pens.splice(i, 1);
+        pen.calculative.canvas.parent.store.pens[p] = undefined;
+      }
+    });
   }
 }
 function onDestroy(pen: any) {
