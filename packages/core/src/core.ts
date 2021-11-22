2c2266c1c9473eefa8b7eee3acddb427d8864739
SignDawn <704425540@qq.com>
2021-11-22 16:10:34 +0800

    events 配置鼠标按下事件

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -733,6 +733,7 @@ export class Topology {
         break;
       case 'click':
         e.pen && e.pen.onClick && e.pen.onClick(e.pen);
+      case 'mousedown':
       case 'dblclick':
         this.store.data.locked && e.pen && this.doEvent(e.pen, eventName);
         break;
