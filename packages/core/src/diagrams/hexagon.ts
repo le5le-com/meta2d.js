daa4261d60ada28b23f5facb77404926780ada5d
SignDawn <704425540@qq.com>
2021-11-29 16:08:34 +0800

    六边形设置锚点去掉

--- packages/core/src/diagrams/hexagon.ts
+++ packages/core/src/diagrams/hexagon.ts
@@ -40,45 +40,45 @@ export function hexagon(pen: Pen, path?: CanvasRenderingContext2D | Path2D) {
   return path;
 }
 
-export function hexagonAnchors(pen: Pen) {
-  const anchors: Point[] = [];
-  anchors.push({
-    id: '0',
-    penId: pen.id,
-    x: 0.25,
-    y: 0,
-  });
+// export function hexagonAnchors(pen: Pen) {
+//   const anchors: Point[] = [];
+//   anchors.push({
+//     id: '0',
+//     penId: pen.id,
+//     x: 0.25,
+//     y: 0,
+//   });
 
-  anchors.push({
-    id: '1',
-    penId: pen.id,
-    x: 0.75,
-    y: 0,
-  });
+//   anchors.push({
+//     id: '1',
+//     penId: pen.id,
+//     x: 0.75,
+//     y: 0,
+//   });
 
-  anchors.push({
-    id: '2',
-    penId: pen.id,
-    x: 1,
-    y: 0.5,
-  });
-  anchors.push({
-    id: '3',
-    penId: pen.id,
-    x: 0.75,
-    y: 1,
-  });
-  anchors.push({
-    id: '4',
-    penId: pen.id,
-    x: 0.25,
-    y: 1,
-  });
-  anchors.push({
-    id: '5',
-    penId: pen.id,
-    x: 0,
-    y: 0.5,
-  });
-  pen.anchors = anchors;
-}
+//   anchors.push({
+//     id: '2',
+//     penId: pen.id,
+//     x: 1,
+//     y: 0.5,
+//   });
+//   anchors.push({
+//     id: '3',
+//     penId: pen.id,
+//     x: 0.75,
+//     y: 1,
+//   });
+//   anchors.push({
+//     id: '4',
+//     penId: pen.id,
+//     x: 0.25,
+//     y: 1,
+//   });
+//   anchors.push({
+//     id: '5',
+//     penId: pen.id,
+//     x: 0,
+//     y: 0.5,
+//   });
+//   pen.anchors = anchors;
+// }
