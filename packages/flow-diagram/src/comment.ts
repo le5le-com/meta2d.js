2dc03291c3570e034266c5963376d58caf9ec29a
SignDawn <704425540@qq.com>
2021-11-30 10:47:31 +0800

    流程图部分图形设置锚点

--- packages/flow-diagram/src/comment.ts
+++ packages/flow-diagram/src/comment.ts
@@ -1,3 +1,4 @@
+import { Point } from '@topology/core';
 import { Pen } from '../../core/src/pen';
 export function flowComment(
   pen: Pen,
@@ -20,4 +21,29 @@ export function flowComment(
   );
 
   return path;
+}
+
+export function flowCommentAnchors(pen: Pen) {
+  const anchors: Point[] = [];
+  anchors.push({
+    id: '0',
+    penId: pen.id,
+    x: 0.25,
+    y: 0,
+  });
+
+  anchors.push({
+    id: '1',
+    penId: pen.id,
+    x: 0.25,
+    y: 1,
+  });
+
+  anchors.push({
+    id: '1',
+    penId: pen.id,
+    x: 0,
+    y: 0.5,
+  });
+  pen.anchors = anchors;
 }
--- packages/flow-diagram/src/document.ts
+++ packages/flow-diagram/src/document.ts
@@ -1,3 +1,4 @@
+import { Point } from '@topology/core';
 import { Pen } from '../../core/src/pen';
 export function flowDocument(
   pen: Pen,
@@ -31,4 +32,36 @@ export function flowDocument(
   );
   path.closePath();
   return path;
+}
+
+export function flowDocumentAnchors(pen: Pen) {
+  const anchors: Point[] = [];
+  anchors.push({
+    id: '0',
+    penId: pen.id,
+    x: 0.5,
+    y: 0,
+  });
+
+  anchors.push({
+    id: '1',
+    penId: pen.id,
+    x: 1,
+    y: 0.5,
+  });
+
+  anchors.push({
+    id: '2',
+    penId: pen.id,
+    x: 0.5,
+    y: 6 / 7,
+  });
+
+  anchors.push({
+    id: '3',
+    penId: pen.id,
+    x: 0,
+    y: 0.5,
+  });
+  pen.anchors = anchors;
 }
