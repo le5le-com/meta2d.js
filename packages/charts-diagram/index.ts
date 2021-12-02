9db6418a7b181c7be944f3a0c6f2e84ff2f39b30
Alsmile <alsmile123@qq.com>
2021-12-02 14:15:09 +0800

    file

--- packages/charts-diagram/index.ts
+++
@@ -1,6 +0,0 @@
-//le5le charts
-export * from './src/lineChart';
-export * from './src/pieChart';
-export * from './src/histogram';
-export * from './src/gauge';
-export * from './src/register';
--- packages/form-diagram/src/commonAnchors.ts
+++
@@ -1,7 +0,0 @@
-import { Pen } from '../../core/src/pen';
-import { Point } from '../../core';
-
-export function commonAnchors(pen: Pen) {
-  const anchors: Point[] = [];
-  pen.anchors = anchors;
-}
