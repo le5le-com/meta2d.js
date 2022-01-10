893485cfca38ed5aefe1a2d78d54a499bcdf95c8
SignDawn <704425540@qq.com>
2022-01-10 15:33:45 +0800

    TopologyData 中存储 svgPaths 中的 path

--- packages/core/src/store/store.ts
+++ packages/core/src/store/store.ts
@@ -36,6 +36,7 @@ export interface TopologyData {
   toArrow?: string;
   lineWidth?: number;
   color?: string;   // 画布默认 color 优先级高于 options.color
+  paths?: { [key: string]: string };  // paths 该图纸使用到的 svgPath 需要保存到 globalStore.paths 中
 }
 
 export enum EditType {
