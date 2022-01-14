d99bb80756067ebc5c1377537e3e8ce2f21c237f
SignDawn <704425540@qq.com>
2022-01-14 15:59:07 +0800

    示例 vue 引入企业图形库

--- examples/vue/src/components/Icons.vue
+++ examples/vue/src/components/Icons.vue
@@ -5,33 +5,39 @@
  * @LastEditTime: 2021-10-13 14:26:48
 -->
 <script setup lang="ts">
-import { icons } from '../utils/data';
-const onDragStart = (e, data) => {
-  e.dataTransfer.setData('Topology', JSON.stringify(data));
+declare const window: Window;
+
+import { nextTick } from "vue";
+import { icons } from "../utils/data";
+const onDragStart = (e: any, data) => {
+  e.dataTransfer.setData("Topology", JSON.stringify(data));
 };
+
+nextTick(() => {
+  // 此处只注册，未将数据放置到工具栏
+  // data.ts 中配置的最后一项即为该图形库中的内容
+  (window as any).registerToolsNew();
+  (window as any).topologyTools = undefined;
+});
 </script>
 
 <template>
-  <div class="aside" >
-    <div class="icon-list" >
-      <div
-        v-for="icon in icons"
-        :key="icon.key"
-      >
+  <div class="aside">
+    <div class="icon-list">
+      <div v-for="icon in icons" :key="icon.key">
         <i
           draggable="true"
           class="iconfont"
-          :class="`icon-${ icon.key }`"
+          :class="`icon-${icon.key}`"
           :title="icon.title"
           @dragstart="onDragStart($event, icon.data)"
         ></i>
       </div>
     </div>
-    <div class="link" >
-      <a href = "http://topology.le5le.com/workspace/">去官网</a>
+    <div class="link">
+      <a href="http://topology.le5le.com/workspace/">去官网</a>
     </div>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
