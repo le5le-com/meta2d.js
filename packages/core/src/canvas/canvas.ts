29be6aeae8959cce02477e85613208ebe4063ada
SignDawn <704425540@qq.com>
2022-02-14 17:41:07 +0800

    ’canvas locked 1 can active

--- packages/core/src/canvas/canvas.ts
+++ packages/core/src/canvas/canvas.ts
@@ -738,7 +738,6 @@ export class Canvas {
 
     // Translate
     if (
-      this.store.data.locked === LockState.DisableEdit ||
       (e.ctrlKey && !this.hoverType) ||
       this.hotkeyType === HotkeyType.Translate ||
       this.mouseRight === MouseRight.TranslateOrContextMenu
