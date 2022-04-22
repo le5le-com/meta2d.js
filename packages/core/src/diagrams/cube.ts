a2ce9599b24a43abfbd30f0f443db196b344933c
SignDawn <704425540@qq.com>
2022-04-22 15:40:26 +0800

    cube z

--- packages/core/src/diagrams/cube.ts
+++ packages/core/src/diagrams/cube.ts
@@ -1,11 +1,13 @@
+import { Pen } from '../pen';
 import { Point } from '../point';
 
-export function cube(ctx: CanvasRenderingContext2D, pen: any) {
+export function cube(ctx: CanvasRenderingContext2D, pen: Pen) {
   let offset = pen.calculative.worldRect.width * 0.25;
-  if ((pen as any).offset > 1) {
-    offset = (pen as any).z;
-  } else if ((pen as any).z > 0) {
-    offset = pen.calculative.worldRect.width * (pen as any).z;
+  const z = (pen as any).z;
+  if (z > 1) {
+    offset = z;
+  } else if (z > 0) {
+    offset = pen.calculative.worldRect.width * z;
   }
 
   const p1 = { x: pen.calculative.worldRect.x, y: pen.calculative.worldRect.y + offset };
@@ -14,7 +16,7 @@ export function cube(ctx: CanvasRenderingContext2D, pen: any) {
   const p4 = { x: pen.calculative.worldRect.x, y: pen.calculative.worldRect.ey };
 
   // front
-  face(ctx, [p1, p2, p3, p4], pen.backgroundFront || pen.background, pen.color);
+  face(ctx, [p1, p2, p3, p4], (pen as any).backgroundFront || pen.background, pen.color);
 
   // up
   face(
@@ -25,7 +27,7 @@ export function cube(ctx: CanvasRenderingContext2D, pen: any) {
       { x: pen.calculative.worldRect.ex, y: pen.calculative.worldRect.y },
       p2,
     ],
-    pen.backgroundUp || pen.background,
+    (pen as any).backgroundUp || pen.background,
     pen.color
   );
 
@@ -38,7 +40,7 @@ export function cube(ctx: CanvasRenderingContext2D, pen: any) {
       { x: pen.calculative.worldRect.ex, y: pen.calculative.worldRect.ey - offset },
       p3,
     ],
-    pen.backgroundRight || pen.background,
+    (pen as any).backgroundRight || pen.background,
     pen.color
   );
 }
