99059fe76290c02a3606c2a796e1a7e4e96746c1
SignDawn <704425540@qq.com>
2022-06-06 18:23:18 +0800

    loadJs; first loadhigh, second high-more

--- packages/chart-diagram/src/register.ts
+++ packages/chart-diagram/src/register.ts
@@ -9,14 +9,11 @@ declare const window: Window;
 export function register(_echarts?: any) {
   echartsList.echarts = _echarts;
   if (!echartsList.echarts && !(window as any).echarts) {
-    const loaderScript = document.createElement('script');
-    loaderScript.type = 'text/javascript';
-    loaderScript.src =
-      'https://cdn.bootcdn.net/ajax/libs/echarts/5.1.2/echarts.min.js';
-    loaderScript.addEventListener('load', () => {
-      topology.render(Infinity);
-    });
-    document.body.appendChild(loaderScript);
+    loadJS(
+      'https://cdn.bootcdn.net/ajax/libs/echarts/5.1.2/echarts.min.js',
+      undefined,
+      true
+    );
   }
   topologyRegister({ echarts });
 }
@@ -24,39 +21,14 @@ export function register(_echarts?: any) {
 export function registerHighcharts(_highcharts?: any) {
   highchartsList.highcharts = _highcharts;
   if (!highchartsList.highcharts && !(window as any).Highcharts) {
-    const loaderScript = document.createElement('script');
-    loaderScript.type = 'text/javascript';
-    loaderScript.src = 'http://cdn.highcharts.com.cn/highcharts/highcharts.js';
-    loaderScript.addEventListener('load', () => {
-      topology.render(Infinity);
+    loadJS('http://cdn.highcharts.com.cn/highcharts/highcharts.js', () => {
+      // load 成功后再引入 more 文件
+      loadJS(
+        'http://cdn.highcharts.com.cn/highcharts/highcharts-more.js',
+        undefined,
+        true
+      );
     });
-    document.body.appendChild(loaderScript);
-
-    const mloaderScript = document.createElement('script');
-    mloaderScript.type = 'text/javascript';
-    mloaderScript.src =
-      'https://cdn.highcharts.com.cn/highcharts/highcharts-more.js';
-    mloaderScript.addEventListener('load', () => {
-      topology.render(Infinity);
-    });
-    document.body.appendChild(mloaderScript);
-    // const loaderScriptexport = document.createElement('script');
-    // loaderScriptexport.type = 'text/javascript';
-    // loaderScriptexport.src =
-    //   'http://cdn.highcharts.com.cn/highcharts/9.2.2/modules/exporting.js';
-    // loaderScriptexport.addEventListener('load', () => {
-    //   topology.render(Infinity);
-    // });
-    // document.body.appendChild(loaderScriptexport);
-
-    // const loaderScriptoffline = document.createElement('script');
-    // loaderScriptoffline.type = 'text/javascript';
-    // loaderScriptoffline.src =
-    //   'http://cdn.highcharts.com.cn/highcharts/9.2.2/modules/offline-exporting.js';
-    // loaderScriptoffline.addEventListener('load', () => {
-    //   topology.render(Infinity);
-    // });
-    // document.body.appendChild(loaderScriptoffline);
   }
   topologyRegister({ highcharts });
 }
@@ -64,13 +36,24 @@ export function registerHighcharts(_highcharts?: any) {
 export function registerLightningChart(_lightningCharts?: any) {
   lightningChartsList.lightningChart = _lightningCharts;
   if (!lightningChartsList.lightningChart && !(window as any).lcjs) {
-    const loaderScript = document.createElement('script');
-    loaderScript.type = 'text/javascript';
-    loaderScript.src = 'js/lcjs.iife.js';  // TODO: 必须放在该路径下的 lcjs 才可以正常使用
-    loaderScript.addEventListener('load', () => {
-      topology.render(Infinity);
-    });
-    document.body.appendChild(loaderScript);
+    loadJS('/js/lcjs.iife.js', undefined, true);
   }
   topologyRegister({ lightningCharts });
+}
+
+/**
+ * 动态添加 script 标签，挂载到 body 上
+ * @param url script 标签的 src 属性
+ * @param callback 引入成功后回调
+ * @param render 是否立即渲染 topology 画布
+ */
+function loadJS(url: string, callback?: () => void, render?: boolean) {
+  const loaderScript = document.createElement('script');
+  loaderScript.type = 'text/javascript';
+  loaderScript.src = url;
+  loaderScript.addEventListener('load', () => {
+    callback?.();
+    render && topology.render(Infinity);
+  });
+  document.body.appendChild(loaderScript);
 }
