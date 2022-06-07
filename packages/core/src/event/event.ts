4ba9c76026c4088298255963db7895113832a46b
SignDawn <704425540@qq.com>
2022-06-07 15:44:12 +0800

    [) and ![)

--- packages/core/src/event/event.ts
+++ packages/core/src/event/event.ts
@@ -1,4 +1,4 @@
-import { IValue } from "../pen";
+import { IValue } from '../pen';
 
 export type EventValue = string | IValue | undefined | null;
 export interface Event {
@@ -23,8 +23,30 @@ export enum EventAction {
 
 export interface Where {
   key?: string;
-  comparison?: string;
+  comparison?: Comparison;
   value?: unknown;
   fn?: Function;
   fnJs?: string;
 }
+
+/**
+ * 触发器中的符号
+ */
+export type Comparison =
+  | '='
+  | '=='
+  | '!='
+  | '>'
+  | '<'
+  | '>='
+  | '<='
+  | '[)' // 介于，数学中的开闭区间
+  | '![)' // 非介于，与上一个相反
+  /**
+   * 属于，类似于 数组的 includes
+   * .. 属于范围语法，30..50 等价于 介于的 [30, 50]
+   * [1, 2, 3]  2 // true  1.5 // false
+   * [1,20,30..50,65] 1 // true 20 // true 30 // true 30.1 // true
+   */
+  | '[]'
+  | '![]'; // 非属于，与上一个相反
