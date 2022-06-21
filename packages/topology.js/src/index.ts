388b687cae8e94652462cd37b491ceb449d2b1b1
anan <2459622492@qq.com>
2022-06-21 17:29:08 +0800

    modify_打包库

--- packages/topology.js/src/index.ts
+++ packages/topology.js/src/index.ts
@@ -1,10 +1,56 @@
 import { Topology } from '@topology/core';
 import { classPens } from '../../class-diagram';
-import { sequencePens } from '../../sequence-diagram';
-import { activityDiagram } from '../../activity-diagram';
+import { sequencePens, sequencePensbyCtx } from '../../sequence-diagram';
+import { activityDiagram, activityDiagramByCtx } from '../../activity-diagram';
+import { flowPens, flowAnchors } from '../../flow-diagram';
+import {
+  register as registerEcharts,
+  registerHighcharts,
+  registerLightningChart,
+} from '../../chart-diagram';
+import { formPens } from '../../form-diagram';
+import { ftaPens, ftaPensbyCtx, ftaAnchors } from '../../fta-diagram';
+import { chartsPens } from '../../le5le-charts';
 
-import { flowPens } from '../../flow-diagram';
 declare const window: any;
 
 window && (window.Topology = Topology);
-export { Topology, classPens, sequencePens, activityDiagram, flowPens };
+window &&
+  (window.registerCommonDiagram = function () {
+    if (window.topology) {
+      registerEcharts();
+      registerHighcharts();
+      registerLightningChart();
+      window.topology.register(flowPens());
+      window.topology.registerAnchors(flowAnchors());
+      window.topology.register(activityDiagram());
+      window.topology.registerCanvasDraw(activityDiagramByCtx());
+      window.topology.register(classPens());
+      window.topology.register(sequencePens());
+      window.topology.registerCanvasDraw(sequencePensbyCtx());
+      window.topology.registerCanvasDraw(formPens());
+      window.topology.registerCanvasDraw(chartsPens());
+      window.topology.register(ftaPens());
+      window.topology.registerCanvasDraw(ftaPensbyCtx());
+      window.topology.registerAnchors(ftaAnchors());
+    }
+  });
+
+export {
+  Topology,
+  classPens,
+  sequencePens,
+  activityDiagram,
+  flowPens,
+  sequencePensbyCtx,
+  activityDiagramByCtx,
+  formPens,
+  ftaPens,
+  ftaPensbyCtx,
+  ftaAnchors,
+  chartsPens,
+  flowAnchors,
+  registerEcharts,
+  registerHighcharts,
+  registerLightningChart,
+};
