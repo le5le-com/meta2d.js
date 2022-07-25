58898eab73b1d11eecb8d81c5bbe4623fa3a0f89
Alsmile <alsmile123@qq.com>
2022-07-25 12:14:24 +0800

    fix bug: polyline

--- packages/core/src/canvas/canvas.ts
+++ packages/core/src/canvas/canvas.ts
@@ -1214,6 +1214,7 @@ export class Canvas {
 
       // 添加点
       const to = getToAnchor(this.drawingLine);
+
       if (to.isTemp) {
         this.drawingLine.calculative.activeAnchor =
           this.drawingLine.calculative.worldAnchors[this.drawingLine.calculative.worldAnchors.length - 2];
@@ -1226,9 +1227,11 @@ export class Canvas {
           penId: to.penId,
         });
       }
-
+      if (this.drawingLineName === 'polyline') {
+        this.drawingLine.autoPolyline = false;
+      }
       this.drawingLine.calculative.drawlineH = undefined;
-      this.drawline();
+      this.drawingLineName !== 'polyline' && this.drawline();
     }
 
     // 单击在节点上，通过自动锚点连线
