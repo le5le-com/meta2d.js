2ff5c28c9bdbb9d9fdfa7ef555cd2d1e11157036
JosephHo <32008dianchenan@163.com>
2022-09-06 17:14:11 +0800

    检测两条线条是否相交，并返回结果

--- packages/core/src/diagrams/line/line.ts
+++ packages/core/src/diagrams/line/line.ts
@@ -396,3 +396,43 @@ export function isBezierIntersectRectangle(from: Point, to: Point, rect: Rect) {
 
   return false;
 }
+
+// 检测两条线段是否相交，并返回结果
+export function checkLineIntersection(line1StartX:number, line1StartY:number, line1EndX:number, line1EndY:number, line2StartX:number, line2StartY:number, line2EndX, line2EndY:number) {
+  // if the lines intersect, the result contains the x and y of the intersection (treating the lines as infinite) and booleans for whether line segment 1 or line segment 2 contain the point
+  let denominator, a, b, numerator1, numerator2, result = {
+      x: null,
+      y: null,
+      onLine1: false,
+      onLine2: false
+  };
+  denominator = ((line2EndY - line2StartY) * (line1EndX - line1StartX)) - ((line2EndX - line2StartX) * (line1EndY - line1StartY));
+  if (denominator == 0) {
+      return result;
+  }
+  a = line1StartY - line2StartY;
+  b = line1StartX - line2StartX;
+  numerator1 = ((line2EndX - line2StartX) * a) - ((line2EndY - line2StartY) * b);
+  numerator2 = ((line1EndX - line1StartX) * a) - ((line1EndY - line1StartY) * b);
+  a = numerator1 / denominator;
+  b = numerator2 / denominator;
+
+  // if we cast these lines infinitely in both directions, they intersect here:
+  result.x = line1StartX + (a * (line1EndX - line1StartX));
+  result.y = line1StartY + (a * (line1EndY - line1StartY));
+/*
+      // it is worth noting that this should be the same as:
+      x = line2StartX + (b * (line2EndX - line2StartX));
+      y = line2StartX + (b * (line2EndY - line2StartY));
+      */
+  // if line1 is a segment and line2 is infinite, they intersect if:
+  if (a > 0 && a < 1) {
+      result.onLine1 = true;
+  }
+  // if line2 is a segment and line1 is infinite, they intersect if:
+  if (b > 0 && b < 1) {
+      result.onLine2 = true;
+  }
+  // if line1 and line2 are segments, they intersect if both of the above are true
+  return result;
+}
