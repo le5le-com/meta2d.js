8863b608c5a37fca2f1eb1a31673dc6e84db00c2
ananzhusen <2459622492@qq.com>
2023-02-08 17:00:26 +0800

    perfect

--- packages/core/src/options.ts
+++ packages/core/src/options.ts
@@ -68,7 +68,7 @@ export interface Options {
   measureTextWidth?: boolean; // 测量文字宽度
   mouseRightActive?: boolean; //是否允许右键选中节点
   disableClipboard?: boolean; //是否禁止系统剪切板
-  drawingLineLength: number; //绘制过程中允许的最大长度，0表示不限
+  drawingLineLength?: number; //绘制过程中允许的最大长度，0表示不限
 }
 
 export const defaultOptions: Options = {
