797632d9343e855749dd2afe3fd5d901b5977ef3
ananzhusen <2459622492@qq.com>
2023-12-18 15:59:26 +0800

    feat:line锚点对齐

--- packages/core/src/canvas/canvas.ts
+++ packages/core/src/canvas/canvas.ts
@@ -1969,7 +1969,7 @@ export class Canvas {
           // Move line anchor
           if (this.hoverType === HoverType.LineAnchor) {
             if (
-              (this.store.active[0].lineName || this.dockInAnchor(e)) &&
+              (this.store.active[0].lineName === 'line' || this.dockInAnchor(e)) &&
               !this.store.options.disableDock &&
               !this.store.options.disableLineDock
             ) {
