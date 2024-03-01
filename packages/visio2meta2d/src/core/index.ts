c772177b094c60206460b8c545ad0c3ad309847e
Grnetsky <1101767893@qq.com>
2024-03-01 17:19:38 +0800

    feat: 移出v2m

--- packages/visio2meta2d/src/core/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @author le5le
- * @description visio vsdx file translate to meta2d file
- * @version 0.0.1
- * */
-export * from "./factory";
-export * from "./types";
-export * from "./map";
