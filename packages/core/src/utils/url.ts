010a33b34377c33ab6498293c94a1f23f99c5c24
ananzhusen <2459622492@qq.com>
2024-05-22 14:03:29 +0800

    feat:queryURLParams

--- packages/core/src/utils/url.ts
+++ packages/core/src/utils/url.ts
@@ -1,5 +1,5 @@
 export function queryURLParams(value?: string) {
-  let url = value || window.location.href.split('?')[1];
+  let url = value || window.location.search.split('?')[1];
   const urlSearchParams = new URLSearchParams(url);
   const params = Object.fromEntries(urlSearchParams.entries());
   return params;
