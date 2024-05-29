58820dcff8d23a3397e7cfa900727cd69cbb2b35
ananzhusen <2459622492@qq.com>
2024-05-29 15:28:21 +0800

    feat:旋转点pivot

--- packages/core/src/pen/model.ts
+++ packages/core/src/pen/model.ts
@@ -365,6 +365,7 @@ export interface Pen extends Rect {
   disabledTextColor?:string; //禁用文字颜色
   inputType?:string; //输入框类型
   deviceId?:string;//关联的设备id
+  pivot?:Point; //旋转中心 
   calculative?: {
     x?: number;
     y?: number;
