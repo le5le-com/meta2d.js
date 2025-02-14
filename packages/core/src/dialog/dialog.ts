8b329d779b6351ba304732fd16f5381fe01de4b8
ananzhusen <2459622492@qq.com>
2025-02-14 16:56:59 +0800

    feat:dialog样式

--- packages/core/src/dialog/dialog.ts
+++ packages/core/src/dialog/dialog.ts
@@ -132,6 +132,12 @@ export class Dialog {
         }`
       );
       sheet.insertRule(
+        `.meta2d-dialog-close svg{
+            width: 18px;
+            height: 18px;
+        }`
+      );
+      sheet.insertRule(
         `.meta2d-dialog-close :hover{
             cursor: pointer;
         }`
@@ -180,10 +186,14 @@ export class Dialog {
       this.title.style.display = 'none';
       this.body.style.height = '100%';
       this.body.style.overflow= 'hidden';
+      this.close.style.top = '18px';
+      this.close.style.right = '20px';
     }else{
       this.dialog.style.padding = '16px 20px';
       this.title.style.display = 'block';
       this.body.style.height = 'calc(100% - 26px)';
+      this.close.style.top = '2px';
+      this.close.style.right = '2px';
     }
     if(rect) {
       this.dialog.style.width = rect.width?(rect.width + 'px'): '80%'
