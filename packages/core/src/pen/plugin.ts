681a39a6202d8007a0f44d826483f954f561392c
Grnetsky <1101767893@qq.com>
2025-11-17 15:35:47 +0800

    fix: 修复rewritePenLifeCycle函数内存泄漏问题

--- packages/core/src/pen/plugin.ts
+++ packages/core/src/pen/plugin.ts
@@ -14,13 +14,13 @@ function rewritePenLifeCycle() {
     if(funcPenMap.has(pen) && funcPenMap.get(pen)){
       funcMap = funcPenMap.get(pen);
     }else {
-      funcPenMap.set(pen,funcMap = new WeakMap());
+      funcPenMap.set(pen,funcMap = new Map());
     }
     if(typeof func !== "function")return ()=>{
       console.warn('[rewritePenLifeCycle] warn: not a function ');
     };
     let funcListSet = new Set();
-    let originFuncMap = new WeakMap(); // 原始事件回调Map
+    let originFuncMap = new Map(); // 原始事件回调Map
     if(funcMap.has(lifeCycle) && funcMap.get(lifeCycle)){
       funcListSet = funcMap.get(lifeCycle);
     }else {
