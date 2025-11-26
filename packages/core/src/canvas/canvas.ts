00e849a1ddfdbf32dd125d139cc8540230dd1a8d
ananzhuen <2459622492@qq.com>
2025-11-26 11:19:21 +0800

    feat:addPens

--- packages/core/src/canvas/canvas.ts
+++ packages/core/src/canvas/canvas.ts
@@ -1589,6 +1589,13 @@ export class Canvas {
     }
     const list: Pen[] = [];
     for (const pen of pens) {
+      if (!pen.id) {
+        pen.id = s8();
+      }
+      !pen.calculative && (pen.calculative = { canvas: this });
+      this.store.pens[pen.id] = pen;
+    }
+    for (const pen of pens) {
       if (this.beforeAddPen && this.beforeAddPen(pen) != true) {
         continue;
       }
